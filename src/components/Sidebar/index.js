82f90de9884234b30584928eb4da2e9a12597bf6
Kamil <you@example.com>
2021-01-08 08:16:23 +0100

    create navbar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { SidebarContainer, Icon, CloseIcon } from "./SidebarElements";
+
+function Sidebar() {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+      </SidebarWrapper>
+      <SideBtnWrap>
+        <SidebarRoute to="/signin">Sign In</SidebarRoute>
+      </SideBtnWrap>
+    </SidebarContainer>
+  );
+}
+
+export default Sidebar;
