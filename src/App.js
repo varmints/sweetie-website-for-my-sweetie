9309ac87204969b4a1139f848636f963e4feec73
Kamil <example@example.com>
2021-04-14 13:11:38 +0200

    white page bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages";
 import SigninPage from "./pages/signin";
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM, { HashRouter } from "react-dom";
+import ReactDOM from "react-dom";
+
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
