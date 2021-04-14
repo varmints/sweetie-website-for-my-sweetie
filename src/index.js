553cba06520cc65b968b2eedff22f9c026336c77
Kamil <example@example.com>
2021-04-14 12:49:43 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { HashRouter } from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
